2db38e2a9741bef72c74f436b5216bd6d1654db7
Alan Bach <kagamibach@gmail.com>
2025-06-03 16:00:43 -0600

    Mission WIP

--- waybionic/src/app/components/Mission.tsx
+++ waybionic/src/app/components/Mission.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Mission() {
   return (
@@ -10,13 +11,29 @@ export default function Mission() {
         <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold text-gray-800 mb-8">
           Our Mission
         </h2>
-        <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-gray-700 leading-relaxed">
-          At <strong>WayBionic</strong>, we strive to revolutionize remote
-          surgery by developing a bionic arm inspired by the challenges of
-          performing medical procedures in space. Our goal is to create
-          innovative, reliable, and precise tools that empower both astronauts
-          and doctors—whether in orbit or on Earth.
-        </p>
+        <div className="relative flex flex-col items-center justify-center">
+          <Image
+            src="/images/est2024.png"
+            alt="Paper Note"
+            width={100}
+            height={100}
+            className="w-28 h-28 mt-8 mx-auto object-contain drop-shadow-lg sm:w-45 sm:h-45 md:w-57 md:h-57 lg:absolute lg:-right-8 lg:bottom-0 lg:-translate-y-1/2 lg:mt-0"
+          />
+          <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-gray-700 leading-relaxed max-w-[16rem] sm:max-w-sm md:max-w-2xl mx-auto px-2">
+            At <strong>WayBionic</strong>, we strive to revolutionize remote
+            surgery by developing a bionic arm inspired by the challenges of
+            performing medical procedures in space. Our goal is to create
+            innovative, reliable, and precise tools that empower both astronauts
+            and doctors—whether in orbit or on Earth.
+          </p>
+          <Image
+            src="/images/pencilbionic.png"
+            alt="WayBionic Mascot with Pencil"
+            width={500}
+            height={500}
+            className="w-28 h-28 mt-8 mx-auto object-contain drop-shadow-lg sm:w-40 sm:h-40 md:w-52 md:h-52 lg:absolute lg:-right-8 lg:bottom-0 lg:translate-y-3/4 lg:mt-0"
+          />
+        </div>
       </div>
     </section>
   );
