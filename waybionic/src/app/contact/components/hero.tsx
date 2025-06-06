a9aefebdab2d6643c323c272b15264f31be1d546
30221616 <adekorede.odebode1@ucalgary.ca>
2025-06-05 23:07:08 -0600

    made all files.

--- waybionic/src/app/contact/components/hero.tsx
+++ waybionic/src/app/contact/components/hero.tsx
@@ -3,13 +3,14 @@
 export default function Hero() {
   return (
     <section
-      className="relative w-full min-h-screen flex flex-col items-center justify-center text-white overflow-hidden"
+      id ="hero" className="relative w-full min-h-screen flex flex-col items-center justify-center text-white overflow-hidden"
       style={{
         backgroundImage: "url('/images/contact_stars_bg.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
+        
       }}
-      id="hero"
+  
     >
       <div className="text-center space-y-4 z-10 px-4">
         <h1 className="text-6xl sm:text-7xl font-bold bg-white text-purple-900 inline-block px-10 py-4 rounded-[10px] tracking-wider shadow-md">
